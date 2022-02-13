5d8023224eeb5e24e75e0934c103626b947546af
umutcrt <98687913+umutcrt@users.noreply.github.com>
2022-02-13 09:05:23 +0300

    Update ViewController.swift

--- webSite/ViewController.swift
+++ webSite/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
-
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    
 class ViewController: UIViewController {
-
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+        
 
 }
-
+        let myURL = URL(string:"https://www.mimolbebek.com/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }}
